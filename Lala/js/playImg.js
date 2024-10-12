fbc025ada89ebbc5fa4b03b585c6d531a8d8f0c7
dat <damtiendat2001@gmail.com>
2024-10-12 18:16:42 +0700

    1

--- Lala/js/playImg.js
+++ Lala/js/playImg.js
@@ -1,4 +1,4 @@
-
+// nút trái tim
 var btn = document.getElementById("heartTxt");
 btn.style.opacity = 0;
 var btnVal = 0;
@@ -14,21 +14,41 @@ function showImage(){
 	}
 }
 
+// khi bấm click trái tim
 function play(){
-	if(t == 0){
-		myImage.setAttribute("src", "");
-		myTxt.innerHTML = "";
-		imageIndex = 0;
-		clearInterval(showImageInterval);
-	}
-	flag = 1 - flag;
-	document.getElementById("typeDiv").style.opacity = flag;
-	document.getElementById("imgTxt").style.opacity = 1 - flag;
-	if(t == 0){
-		//setTimeout(showImage, 1000);
-		setInterval(showImage, 2500);
-	}
-	t++;
+	
+	// if(t == 0){
+	// 	myImage.setAttribute("src", "");x
+	// 	myTxt.innerHTML = "";
+	// 	imageIndex = 0;
+	// 	clearInterval(showImageInterval);
+	// }
+	// flag = 1 - flag;
+	// document.getElementById("typeDiv").style.opacity = flag;
+	// document.getElementById("imgTxt").style.opacity = 1 - flag;
+	// if(t == 0){
+	// 	//setTimeout(showImage, 1000);
+	// 	// chuyển ảnh
+	// 	setInterval(showImage, 2500);
+	// }
+	// t++;
+
+	//-----------
+	// if (t == 0) {
+    //     myImage.setAttribute("src", "");
+    //     myTxt.innerHTML = "";
+    //     imageIndex = 0;
+    // }
+
+    // flag = 1 - flag;
+    // document.getElementById("typeDiv").style.opacity = flag;
+    // document.getElementById("imgTxt").style.opacity = 1 - flag;
+
+    // Gọi showImage mỗi khi nhấn nút, thay vì sử dụng setInterval
+	document.getElementById("typeDiv").style.opacity = 0;
+    showImage();
+
+    // t++;
 }
 
 function preshowImage(){
@@ -72,4 +92,13 @@ var showImageInterval;
 var imgInterval;
 var buttonInterval;
 
-event();
+
+// 1 time mới hiện nút tim
+imgInterval = setInterval(function (){
+	if(ok == 3){
+		setTimeout(function(){buttonInterval = setInterval(buttonFadeIn, 50);}, 1500);
+		clearInterval(imgInterval);
+	
+	}
+}, 50);
+// event();
