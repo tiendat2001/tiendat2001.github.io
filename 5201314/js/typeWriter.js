36947ee6c1c163b68d8a57b771d45ddd19796dfa
tiendat2001 <75568539+tiendat2001@users.noreply.github.com>
2024-11-02 11:31:27 +0700

    1

--- 5201314/js/typeWriter.js
+++ 5201314/js/typeWriter.js
@@ -1,7 +1,7 @@
 
 let i = 0;
-let text1 = "Helluu thế là t vs m cũng quen nhau đc gần 4 năm r nhỉ";
-let text2 = "Cùng xem lại kỷ niêm nhaa."
+let text1 = "Helluu thế là t vs m cũng quen nhau đc gần 4 năm r nhỉ.";
+let text2 = "Cùng xem lại kỷ niệm nhaa."
 let speed = 1; // dang chinh
 
 function typeWriter(text, para){
